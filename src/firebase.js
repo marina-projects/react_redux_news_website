0a3c406d52d8d8c45357774025275b5222984d96
Марина Романова <marina.simaganova@gmail.com>
2024-06-26 16:05:16 +0200

    prepare to netlify

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7yPqZRyniLfOjYdDWSm_GhdgCTMgfjr8",
-  authDomain: "news-website-react-redux.firebaseapp.com",
-  projectId: "news-website-react-redux",
-  storageBucket: "news-website-react-redux.appspot.com",
-  messagingSenderId: "107933684893",
-  appId: "1:107933684893:web:4059027c762fa590adb074"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -25,6 +19,5 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const Timestamp = firebase.firestore.Timestamp;
-
 
 export { db, storage, Timestamp };
